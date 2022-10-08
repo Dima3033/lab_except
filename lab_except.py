603fa920b704c5e71c9ebe8f4da687f8b0bc5fea
Dima <livitorskipunovich@gmail.com>
2022-10-08 10:58:12 +0300

    Update: Add ' Завданя 1'

--- lab_except.py
+++ lab_except.py
@@ -2,15 +2,11 @@ try:
     num_1 = int(input("Введіть число 1 : "))
     num_2 = int(input('Введіть число 2 : '))
     sum = 0
-    counter = 0
+    counter = num_2-num_1+1
     for i in range(num_1, num_2 + 1):
-        print(f'{sum} +{i} = ', end=' ')
         sum += i
-        print(f'{sum}')
-        print(f'Sum = {sum}')
-        counter+=1
-    s = (num_1+num_2)/counter
-    print(s)
+    ar = sum/counter
+    print(f'{sum} \n {ar}')
 except Exception as ex:
     print(f'Erorr information: {ex}')
 finally:
